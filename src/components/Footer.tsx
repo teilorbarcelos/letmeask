05da657ce20f4300aa2ce7f167cc7abb9103d094
Teilor Souza Barcelos <tsb-@live.com>
2021-08-10 16:47:23 -0300

    removed enter room that already exists option

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 import '../styles/footer.scss'
 export function Footer() {
   return (
-    <footer className="global-footer">
+    <footer className="global-footer mt-8">
       <p>Developed by <a target="_blank" rel="noreferrer" href="https://github.com/teilorbarcelos">Teilor Souza Barcelos</a> &copy; 2021</p>
     </footer>
   )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useHistory} from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
@@ -12,58 +12,62 @@ import { Footer } from '../components/Footer'
 import { LogoutButton } from '../components/LogoutButton'
 import { WelcomeMsg } from '../components/WelcomeMsg'
 import { SwitchButton } from '../components/SwitchButton'
+import { Illustration } from '../components/svg/Illustration'
 
 export function Home() {
   const history = useHistory();
-  const { signInWithGoogle, user} = useAuth();
+  const { signInWithGoogle, user } = useAuth();
   const [roomCode, setRoomCode] = useState('');
 
   async function handleCreateRoom() {
-    if(!user){
+    if (!user) {
       await signInWithGoogle();
     }
     history.push('/rooms/new')
   }
 
-  async function handleJoinRoom(event: FormEvent) {
-    event.preventDefault();
+  // async function handleJoinRoom(event: FormEvent) {
+  //   event.preventDefault();
 
-    if(roomCode.trim() === ''){
-      return;
-    }
+  //   if (roomCode.trim() === '') {
+  //     return;
+  //   }
 
-    const roomRef = await database.ref(`rooms/${roomCode}`).get();
-    if(!roomRef.exists()){
-      alert('Room does not exists!');
-      return;
-    }
+  //   const roomRef = await database.ref(`rooms/${roomCode}`).get();
+  //   if (!roomRef.exists()) {
+  //     alert('Room does not exists!');
+  //     return;
+  //   }
 
-    if(roomRef.val().endedAt){
-      alert('Room already closed!')
-      return;
-    }
+  //   if (roomRef.val().endedAt) {
+  //     alert('Room already closed!')
+  //     return;
+  //   }
 
-    history.push(`/rooms/${roomCode}`);
-  }
+  //   history.push(`/rooms/${roomCode}`);
+  // }
 
   return (
     <div id="page-auth">
       <aside>
-        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+
+        <Illustration />
+
+        {/* <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" /> */}
         <strong>Crie salas de Q&amp;A ao vivo</strong>
         <p>Tire as dúvidas da sua audiência em tempo real</p>
       </aside>
       <main>
-          <SwitchButton />
-          <div className="main-content">
-            <img src={logoImg} alt="Letmeask" />
-            <WelcomeMsg />
-            <button onClick={handleCreateRoom} className="create-room">
-              <img src={googleIconImg} alt="Logo do Google" />
-              Crie sua sala com o Google
-            </button>
-            <LogoutButton />
-            <div className="separator">ou entre em uma sala</div>
+        <SwitchButton />
+        <div className="main-content">
+          <img src={logoImg} alt="Letmeask" />
+          <WelcomeMsg />
+          <button onClick={handleCreateRoom} className="create-room">
+            <img src={googleIconImg} alt="Logo do Google" />
+            Crie sua sala com o Google
+          </button>
+          <LogoutButton />
+          {/* <div className="separator">ou entre em uma sala</div>
             <form onSubmit={handleJoinRoom}>
               <input
                 type="text"
@@ -72,9 +76,9 @@ export function Home() {
                 value={roomCode}
               />
               <Button type="submit">Entrar na sala</Button>
-            </form>
-          </div>
-        <Footer/>
+            </form> */}
+        </div>
+        <Footer />
       </main>
     </div>
   )
